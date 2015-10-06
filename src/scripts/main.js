7e9ac623dcb0562e63ee31a95dfb1b506d4eeecc
Konstantin Zakablukovsky <2cynicus@gmail.com>
2015-10-06 16:36:08 +1000

    Add slider, mobile header, menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,61 @@
 $(document).ready(function(){
 
+	/*
+	 * Slider
+	 */
+	var slider = $('.slider');
+	var sliderHeight = 0;
+	$('.slider__item img').load().each(function(){
+		if($(this).height() > sliderHeight){sliderHeight = $(this).height();}
+	});
+	$('.slider').height(sliderHeight);
+
+	var interval = 5000;
+	var timer;
+	var sliderItem = $('.slider__item');
+	var sliderLength = sliderItem.length;
+	var sliderWidth = sliderItem.eq(0).width();
+	var currentItem = 0;
+	var nextItem;
+
+	timer = setInterval(nextSlide, interval);
+
+	$('.slider__nav--prev').click(function(){
+		prevSlide();
+		clearInterval(timer);
+		timer = setInterval(nextSlide, interval);
+	});
+	$('.slider__nav--next').click(function(){
+		nextSlide();
+		clearInterval(timer);
+		timer = setInterval(nextSlide, interval);
+	});
+
+	function prevSlide(){
+		if (currentItem === 0){
+			nextItem = sliderLength - 1;
+		}
+		else{
+			nextItem = currentItem - 1;
+		}
+		sliderItem.eq(nextItem).css({'left': sliderWidth * -1});
+		sliderItem.each(function(){
+			$(this).animate({'left': parseInt($(this).css('left')) + sliderWidth });
+		});
+		currentItem = nextItem;
+	}
+	function nextSlide(){
+		if(currentItem < (sliderLength - 1)){
+			nextItem = currentItem + 1;
+		}
+		else{
+			nextItem = 0;
+		}
+		sliderItem.eq(nextItem).css({'left': sliderWidth});
+		sliderItem.each(function(){
+			$(this).animate({'left': parseInt($(this).css('left')) - sliderWidth });
+		});
+		currentItem = nextItem;
+	}
+
 });
